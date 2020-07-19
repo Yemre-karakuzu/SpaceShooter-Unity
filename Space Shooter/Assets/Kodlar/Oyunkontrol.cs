733d813552476164338dadafd5b3efb600d037b6
Yemre-karakuzu <yunus11414@gmail.com>
2020-07-19 23:48:13 +0300

    unityle kodlandı

---
+++ Space Shooter/Assets/Kodlar/Oyunkontrol.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oyunkontrol : MonoBehaviour {
+
+    public GameObject Asteroid;
+    public Vector3 randomPos;
+    public float baslangıcbekeme;
+    public float olusturmabekleme;
+    public float dongubekleme;
+    int score;
+	void Start ()
+    {
+        score = 0;
+        StartCoroutine(olustur());	
+	}
+    IEnumerator olustur()
+    {
+        yield return new WaitForSeconds(baslangıcbekeme);
+        while (true)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Vector3 vec = new Vector3(Random.Range(-randomPos.x, randomPos.x), 0, randomPos.z);
+                Instantiate(Asteroid, vec, Quaternion.identity);
+                yield return new WaitForSeconds(olusturmabekleme);
+            }           
+            yield return new WaitForSeconds(dongubekleme);    
+        }
+        
+        
+    }
+    public void ScoreArttır()
+    {
+
+    }
+}
---
+++ Space Shooter/Assets/Kodlar/TemaslaYokOl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemaslaYokOl : MonoBehaviour {
+
+    public GameObject patlama;
+    public GameObject playerPatlama;
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag!="Sınır")
+        {
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+            Instantiate(patlama, transform.position, transform.rotation);
+        }
+        if (col.tag=="Player")
+        {
+            Instantiate(patlama, col.transform.position,col.transform.rotation);
+        }
+    }
+
+
+}
